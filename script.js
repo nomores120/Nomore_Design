7e117c979ec457403627a24b89cfbdfc1cca036b
nomores120 <nomores120@gmail.com>
2024-08-08 09:56:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add event listener to navigation links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
